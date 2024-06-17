72241b38fad0b22de0ce70d1d268819ead63297a
Rasekaran Atputharajah <rasekaran@Rasekarans-MacBook-Pro.local>
2024-06-17 13:11:32 +0530

    [chore]: Initial project setup

---
+++ src/main/java/com/assignment/book_lib_api/BookLibApiApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.book_lib_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLibApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLibApiApplication.class, args);
+	}
+
+}
