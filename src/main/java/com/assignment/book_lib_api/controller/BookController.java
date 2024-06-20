f979b41a8aa76daebc3a5bda1d3646b5938daba9
rasekaran <rasikaran1984@gmail.com>
2024-06-20 08:23:58 +0530

    fix: enable cors for all URLs

--- src/main/java/com/assignment/book_lib_api/controller/BookController.java
+++ src/main/java/com/assignment/book_lib_api/controller/BookController.java
@@ -11,6 +11,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class BookController {
 
     @Autowired
